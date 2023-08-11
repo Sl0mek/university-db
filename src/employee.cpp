482c78e8a18597b2c371d694df9edceb268da563
Andrzej Slomka <andrzejslomkapraca@gmail.com>
2023-08-11 22:44:12 +0200

    Added employee.cpp and employee.hpp files

---
+++ src/employee.cpp
@@ -0,0 +1,99 @@
+#include "employee.hpp"
+#include <sstream>
+#include <vector>
+
+Employee::Employee() {
+}
+
+Employee::Employee(std::string employee) {
+    std::vector<std::string> properties;
+    std::stringstream ss(employee);
+    std::string property;
+
+    while (std::getline(ss, property, ';')) {
+        properties.push_back(property);
+    }
+
+    Gender gender = Gender::Unknown;
+
+    if (properties[4] == "Male") {
+        gender = Gender::Male;
+    } else if (properties[4] == "Female") {
+        gender = Gender::Female;
+    }
+
+    Address address(properties[5],
+                    properties[6],
+                    properties[7],
+                    properties[8],
+                    properties[9],
+                    properties[10],
+                    properties[11]);
+    this->firstName_ = properties[0];
+    this->lastName_ = properties[1];
+    this->address_ = address;
+    this->salary_ = std::stod(properties[2]);
+    this->pesel_ = properties[3];
+    this->gender_ = gender;
+}
+
+Employee::Employee(const std::string& firstName,
+                 const std::string& lastName,
+                 const Address& address,
+                 const std::string& pesel,
+                 Gender gender,
+                 double salary)
+    : Person(firstName, lastName, address, pesel, gender),
+      salary_(salary) {}
+
+Employee::~Employee()
+{
+
+}
+
+std::string Employee::getDescription() const {
+    std::stringstream ss;
+    ss << "==================================\n";
+    ss << "Name: " << firstName_ << " " << lastName_ << "\n";
+    ss << "Address: " << address_.getAddress() << "\n";
+    ss << "PESEL: " << pesel_ << "\n";
+    ss << "Gender: " << getGenderString() << "\n";
+    ss << "Salary: " << salary_ << "\n";
+    ss << "==================================\n";
+
+    return ss.str();
+}
+
+double Employee::getSalaryFromUser() {
+    double salary;
+    std::cout << "Enter salary: \n> ";
+    std::cin >> salary;
+    return salary;
+}
+
+void Employee::initDataFromUser() {
+    this->firstName_ = getFirstNameFromUser();
+    this->lastName_ = getLastNameFromUser();
+    this->pesel_ = getPeselFromUser();
+    this->gender_ = getGenderFromUser();
+    this->address_.initDataFromUser();
+    this->salary_ = getSalaryFromUser();
+}
+
+std::string Employee::toString() {
+    return this->firstName_ + ";" +
+           this->lastName_ + ";" +
+           std::to_string(this->salary_) + ";" +
+           this->pesel_ + ";" +
+           getGenderString() + ";" +
+           this->address_.toString();
+}
+
+double Employee::getSalary() const {
+    return salary_;
+}
+
+bool Employee::setSalary(double salary) {
+    salary_ = salary;
+    return true;
+}
---
+++ src/employee.hpp
@@ -0,0 +1,32 @@
+#ifndef EMPLOYEE_HPP
+#define EMPLOYEE_HPP
+
+#include <iostream>
+#include <string>
+#include "address.hpp"
+#include "person.hpp"
+
+class Employee : public Person{
+private:
+    double salary_;
+    double getSalaryFromUser();
+
+public:
+    Employee();
+    Employee(std::string student);
+    Employee(const std::string& firstName,
+            const std::string& lastName,
+            const Address& address,
+            const std::string& pesel,
+            Gender gender,
+            double salary);
+    ~Employee() override;
+
+    std::string getDescription() const override;
+    std::string toString() override;
+    void initDataFromUser() override;
+    double getSalary() const override;
+    bool setSalary(double salary) override;
+};
+
+#endif // EMPLOYEE_HPP
