6ec3ebdc8057e0e7acb7a1dac0897ecd5b41df4b
Andrzej Slomka <andrzejslomkapraca@gmail.com>
2023-08-13 22:49:24 +0200

    Added program parameterization

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,21 @@
 #include "db.hpp"
 
 int main(int argc, char const* argv[]) {
-    Db db;
-    db.displayMenu();
+    if (argc == 1) {
+        Db db("db.txt");
+        db.displayMenu();
+    } else if (argc == 2) {
+        std::string arg1 = argv[1];
+        if (arg1 == "--generate" || arg1 == "-g") {
+            Db db;
+            db.displayMenu();
+        } else {
+            std::cout << "Usage: " << argv[0] << " [--generate | -g]" << std::endl;
+            return 1;
+        }
+    } else {
+        std::cout << "Usage: " << argv[0] << " [--generate | -g]" << std::endl;
+        return 1;
+    }
     return 0;
 }
